4a32f76a5baa6b475503a6becfa0258915052218
Tobiascph <tobias.zimmer@hotmail.com>
2020-04-27 12:25:39 +0200

    Hejsax4

--- src/Data/Script.sql
+++ src/Data/Script.sql
@@ -1,21 +1,32 @@
-create database if not exists pizzeria;
+USE pizzeria;
+
 create table pizzaer (
-	PizzaID int not null auto_increment,
-    Pizzanavn varchar(255) default null,
-    Ingredienser varchar(255) default null,
-    Pris double default null,
-    primary key(PizzaID)
-    )engine=InnoDB default charset=utf8;
+PizzaID int not null auto_increment,
+Pizzanavn varchar(255) default null,
+Ingredienser varchar(255) default null,
+Pris double default null,
+primary key(PizzaID)
+)engine=InnoDB default charset=utf8;
 insert into pizzaer(Pizzanavn,Ingredienser,Pris)
 values ('Vesuvio','tomatsauce,ost,skinke og oregano',57);
 
+insert into pizzaer(Pizzanavn,Ingredienser,Pris)
+values ('Margarita','tomatsauce,ost,skinke og oregano',57);
+
+
+insert into pizzaer(Pizzanavn,Ingredienser,Pris)
+values ('Pineapple','tomatsauce,ost,skinke og oregano',57);
+
 create table bestillinger (
 BestillingsID int not null auto_increment,
-Afhentning time default null,
+Afhentning TIME,
 primary key(BestillingsID)
 )engine=InnoDB default charset=utf8;
-insert into bestillinger(afhentning)
-values(122200);
+insert into bestillinger(Afhentning)
+values(`11:22:00`);
+
+INSERT INTO bestillinger(Afhentning)
+VALUES(`14:30:00`);
 
 create table pizzabestillinger (
 BestillingsID int not null,
@@ -23,3 +34,6 @@ PizzaID int not null,
 foreign key(PizzaID) references pizzaer(PizzaID),
 foreign key(BestillingsID) references bestillinger(BestillingsID)
 )engine=InnoDB default charset=utf8;
+
+INSERT INTO pizzabestillinger (BestillingsID, PizzaID)
+VALUES(1,3),(1,2),(2,1);
